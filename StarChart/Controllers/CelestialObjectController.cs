eb4f06efb0d6f94aadb11c199646c06b353cbfee
Payal Kumari <payalkumari24195@gmail.com>
2019-05-17 12:02:25 +0530

    Changes
    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class ClestialObjectController : ControllerBase
+    public class CelestialObjectController
     {
         private readonly ApplicationDbContext _context;
 
-        public ClestialObjectController(ApplicationDbContext context)
+        public CelestialObjectController(ApplicationDbContext context)
         {
-            _context = context;
+            _context = context; 
         }
     }
 }
