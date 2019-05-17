d72c4122c675c921d68455e2221302a1dbc6d0d2
Payal Kumari <payalkumari24195@gmail.com>
2019-05-17 11:45:10 +0530

    Create ClestialObjectController.cs
    Completed the module

---
+++ StarChart/Controllers/ClestialObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ClestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
